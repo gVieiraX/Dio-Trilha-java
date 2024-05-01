71a48f007f4ce84eb869d9bb3608ab7af2d65a9f
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-05-01 17:12:01 -0300

    att1

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class SmartTv {
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal( int novoCanal){
+        canal = novoCanal;
+    }
+    public void subirCanal(){
+        canal++;
+    }
+    public void descerCanal(){
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+
+    }
+
+    public void ligar(){
+        ligada = true;
+
+    }
+    public void desligar(){
+        ligada = false;
+
+    }
+
 }
