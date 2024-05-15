8ab1c7bc04ed4e33962545c70bf270e656796fae
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-05-15 17:29:14 -0300

    get/set

---
+++ GET-SET/GET-SET/src/escola/Esola.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class Esola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.setNome("Felipe");
+        felipe.setIdade(12);
+        felipe.setSexo("Masculino");
+
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
+    }
+}
+    
+
