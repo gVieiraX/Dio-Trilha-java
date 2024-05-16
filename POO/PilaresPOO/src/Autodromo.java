d80c9b41b89ca8e7ffb7bea79823004dc11147df
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-05-16 19:14:49 -0300

    PilaresPOO

--- POO/PilaresPOO/src/Autodromo.java
+++ POO/PilaresPOO/src/Autodromo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Autodromo {
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro bmw = new Carro();
+        bmw.setChassi("242424");
+        // bmw.ligar();
+
+        Moto xre300 = new Moto();
+        xre300.setChassi("33123");
+      //  xre300.ligar();
+
+        Veiculo coringa = xre300;
+        coringa.ligar();
+        }
 }
