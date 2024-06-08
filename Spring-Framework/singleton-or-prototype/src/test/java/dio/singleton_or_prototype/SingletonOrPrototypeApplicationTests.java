fc07c1c8afff7dfe0b8df9fc5b266cc6f86a16e1
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-06-07 22:17:47 -0300

    singleton or prototype

---
+++ Spring-Framework/singleton-or-prototype/src/test/java/dio/singleton_or_prototype/SingletonOrPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.singleton_or_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingletonOrPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
