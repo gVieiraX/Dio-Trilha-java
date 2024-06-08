29c1efabac25c436353d25a8ab7c63e08caf1ca4
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-06-07 22:46:22 -0300

    configuration properties

--- Spring-Framework/configuration-properties/src/main/java/dio/configuration_properties/App/SistemaMensagem.java
+++ Spring-Framework/configuration-properties/src/main/java/dio/configuration_properties/App/SistemaMensagem.java
@@ -1,23 +1,20 @@
 package dio.configuration_properties.App;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-    @Value("${remetente.nome}")
-    private String nome;
-    private String email;
-    private List<Long> telefone;
-
+    @Autowired
+    private Remetente remetente;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome
-                + "\nE-mail:" + email
-                + "\nCom telefones para contato: " + telefone);
+        System.out.println("Mensagem enviada por: " + remetente.getNome()
+                + "\nE-mail:" + remetente.getEmail()
+                + "\nCom telefones para contato: " + remetente.getTelefone());
         System.out.println("Seu cadastro foi aprovado");
     }
 }
