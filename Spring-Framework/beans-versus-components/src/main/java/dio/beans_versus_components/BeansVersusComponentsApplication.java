169e2e223f82fc0ee54001053b60b48465a95126
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-06-07 22:14:21 -0300

    beans vs components

---
+++ Spring-Framework/beans-versus-components/src/main/java/dio/beans_versus_components/BeansVersusComponentsApplication.java
@@ -0,0 +1,25 @@
+package dio.beans_versus_components;
+
+import dio.beans_versus_components.App.ConversorJson;
+import dio.beans_versus_components.App.ViaCepResponse;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BeansVersusComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansVersusComponentsApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
+
+}
