73e6636090f9774b1cdecb9aa04a66f461e81188
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-06-08 00:23:11 -0300

    postgres conexion

--- Spring-Framework/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ Spring-Framework/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -6,16 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
+
     @Override
     public void run(String... args) throws Exception {
+        List<User> users = repository.findByNameContaining("Gabriel");
+        for(User u: users){
+            System.out.println(u);
+        }
+    }
+    private void insertUser(){
+
         User user = new User();
         user.setName("Gabriel");
         user.setUsername("Vieira");
         user.setPassword("dio124");
+
+        user.setName("Gabriel ");
+        user.setUsername("gvr");
+        user.setPassword("dio125");
+
+        user.setName("Pe Rosa");
+        user.setUsername("gvsr");
+        user.setPassword("dio126");
 
         repository.save(user);
 
