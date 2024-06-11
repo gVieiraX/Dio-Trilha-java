d89dd5d90ea051b597644a1c04b4cc07933fa919
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-06-10 22:15:30 -0300

    spring security with jwt

---
+++ Spring-Framework/dio-spring-security-jwt/src/main/java/dio/spring_security_jwt/DioSpringSecurityJwtApplication.java
@@ -0,0 +1,15 @@
+package dio.spring_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DioSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringSecurityJwtApplication.class, args);
+
+	}
+
+}
---
+++ Spring-Framework/dio-spring-security-jwt/src/main/java/dio/spring_security_jwt/security/UserService.java
@@ -0,0 +1,23 @@
+package dio.spring_security_jwt.security;
+
+import dio.spring_security_jwt.model.User;
+import dio.spring_security_jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public void createUser(User user) {
+        String pass = user.getPassword();
+        // Criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
