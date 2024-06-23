b6297aeb6b847ad03cec5f1a333d505be3110779
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-06-23 17:43:38 -0300

    Implementação da camada de domínio

---
+++ Spring-Framework/apirest-na-nuvem/src/main/java/one/digitalinnovation/apirest_na_nuvem/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.apirest_na_nuvem.domain.repository;
+
+import one.digitalinnovation.apirest_na_nuvem.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+}
